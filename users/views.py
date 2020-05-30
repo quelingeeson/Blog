81df19d8378a9f44803cc6ce2786ca6f6c5dc9c9
quelin <you.com>
2020-05-30 18:16:06 +0530

    Working login and logout

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == "POST":
@@ -9,7 +10,11 @@ def register(request):
             form.save()
             username=form.cleaned_data.get('username')
             messages.success(request,f'account created for-{username}')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form=UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
